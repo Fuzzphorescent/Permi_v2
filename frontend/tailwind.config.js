7d4a92e304275b23a0ccba7d3d274c24b3a7519a
Fuzzy <fuzzphorescence@gmail.com>
2023-04-16 20:41:29 +0530

    Configured tailwind themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      'grey': '#161C1D',
+      'black': '#0A0B0C',
+      'yellow': '#EC9A29',
+      'teal': '#0B485B',
+    },
+    fontFamily: {
+      serif: ['Philosopher', 'serif'],
+     },
+    extend: {
+      fontFamily: {
+        'philo': ['Philosopher', 'serif'],
+        'canon': ['"IM Fell French Canon SC"', 'serif'],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
-  plugins: [],
+  plugins: [], 
 }
