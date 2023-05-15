0192cd873e951340c9be443794c79a66677adfaf
Fuzzy <fuzzphorescence@gmail.com>
2023-05-15 18:14:46 +0530

    Design the dashboard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,12 +1,13 @@
-// import Link from 'next/link';
-// import Main from './components/menu'
-// import useRouter from 'next/router'
+import Link from 'next/link';
+import Main from './components/menu'
+import useRouter from 'next/router'
 
-// export default function Dashboard() {
+export default function Dashboard() {
 
-//   const router = useRouter;
+  const router = useRouter;
 
-//   return (
-    
-//   )
-// }
+  return (
+    <div className="flex h-screen flex-col font-philo text-yellow">
+      <div className=" flex flex-row w-screen h-20 justify-between flex-row font-color bg-black text-yellow text-6xl"></div>
+  )
+}
